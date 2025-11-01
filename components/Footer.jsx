9e943ef14b98e4dcfe9718c8c87c3ddbc62cf80e
Murat <kasimrize@gmail.com>
2025-11-01 16:12:07 +0300

    Delete components/Footer.jsx

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="mt-16 border-t">
-      <div className="container py-8 text-sm text-neutral-600">
-        © {new Date().getFullYear()} Sahneva. Tüm hakları saklıdır.
-      </div>
-    </footer>
-  );
-}
