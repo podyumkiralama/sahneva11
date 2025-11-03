fb1e0e7190fc088e240676a591fa4df1d4c7c957
Murat <kasimrize@gmail.com>
2025-11-03 16:02:20 +0300

    Create ga-init.js

---
+++ public/ga-init.js
@@ -0,0 +1,6 @@
+/* public/ga-init.js */
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+// App Router’da çift sayımı kapat
+gtag('config', 'G-J5YK10YLLC', { send_page_view: false, anonymize_ip: true });
