3640cffcbd200f400c45dc4a10cb5e7a033daabf
Murat <kasimrize@gmail.com>
2025-11-03 16:16:23 +0300

    Update ga-init.js

--- public/ga-init.js
+++ public/ga-init.js
@@ -2,5 +2,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){ dataLayer.push(arguments); }
 gtag('js', new Date());
-// App Router’da çift sayımı kapat
 gtag('config', 'G-J5YK10YLLC', { send_page_view: false, anonymize_ip: true });
