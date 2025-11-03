14963e0eb164095282a8ef6170394b64c3fa6579
Murat <kasimrize@gmail.com>
2025-11-03 16:52:19 +0300

    Update ga-init.js

--- public/ga-init.js
+++ public/ga-init.js
@@ -1,5 +1,27 @@
-/* public/ga-init.js */
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-J5YK10YLLC', { send_page_view: false, anonymize_ip: true });
+/* GA init – inline yerine harici dosya */
+(function () {
+  try {
+    const gaId =
+      document.currentScript?.getAttribute("data-ga-id") ||
+      (typeof process !== "undefined" && process.env && process.env.NEXT_PUBLIC_GA_ID) ||
+      "G-J5YK10YLLC";
+
+    if (!gaId) return;
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+
+    gtag("js", new Date());
+    gtag("config", gaId, {
+      anonymize_ip: true,
+      allow_google_signals: false,
+      allow_ad_personalization_signals: false,
+      transport_type: "beacon",
+    });
+  } catch (e) {
+    // Sessizce geç
+  }
+})();
