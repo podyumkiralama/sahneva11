df6350337b9ea7c7ecd0a4f70fcd65dc61a07efe
Murat <kasimrize@gmail.com>
2025-11-04 19:36:05 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      // theme konfigürasyonunuz
-    },
+    extend: {},
   },
-  plugins: [
-    // plugin'leriniz
-  ],
+  plugins: [],
 }
