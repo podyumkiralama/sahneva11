fcd2974ebf601dca926fb91af846e6cd951416ac
Murat <kasimrize@gmail.com>
2025-11-06 14:09:58 +0300

    Fix middleware matcher literal

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [MISSING_FONT_PATH],
+  matcher: ["/_next/static/media/83afe278b6a6bb3c.p.3a6ba036.woff2"],
 };
